9eff9dc65ab73a97cca383fc7d138883ab08c3b0
Rickie Anand <mumbai85@gmail.com>
2020-05-17 13:03:16 +0530

    create cards for memorising

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import staticData from './data';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const data = [...staticData]
+	const [cards, setCards] = useState([])
+	const [myData, setMyData] = useState(data)
+	useEffect(()=>{
+		getRandomCards()
+	}, [])
+
+
+	const getRandomCards = () => {
+		console.log('myData', myData)
+		console.log('data', data)
+		if(myData.length===0) {
+			setMyData(data)
+		}
+		const temp = myData.sort(function() {
+			return .5 - Math.random();
+		}).splice(0, 5)
+		console.log('temp', temp)
+		//const temp = myData[Math.floor(Math.random() * myData.length)]
+		setCards(temp)
+	}
+
+
+	console.log('cards', cards)
+
+	return (
+		<div className="App">
+			
+			<header className="header"> 
+				<h1>ANATOMICAL TERMS</h1>
+				<h2>THEIR ORIGIN AND DERIVATION</h2>
+			</header>      
+			<section className='card-section'>
+				<div className='card-container'>
+					{
+						cards.length
+						?
+							cards.map((record, i) => {
+								const bgColor = record?.type ? 'orange' : (record?.origin === 'Latin' ? 'red' : 'green')
+								const id = record.id
+								return (
+									<div
+										className='card'
+										key={i}
+										style={{background: bgColor}}
+									>
+										<h1>{record.term}</h1>
+										<div className='derivation'>{record.derivation}</div>
+									</div>
+								)
+							})
+						:
+							<div className='no-cards' onClick={getRandomCards}>
+								Congratuations!!! You have read all the cards.
+							</div>
+					}
+				</div>
+			</section>
+			<div className='random5' onClick={getRandomCards}>+5</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,64 @@
+const staticData = [{
+            id: 1,
+            term: "A Abbe, . Ernst.",
+            derivation: "184o-1905. German mathematical-physicist who devised the oil immersion lens (1878) and the condenser system in the microscope which com-monly bears his name (i886).",
+            type: "German mathematical-physicist",
+            lifespan: "1840-1905"
+        }, {
+            id: 2,
+            term: "Abdomen",
+            derivation: "genitive abdominis. Etymology obscure, either from abdere (L) to conceal, or from adeps (L) fat. The termination—omen may be associated with the practice by ancient augurs of examining the abdominal contents in order to foretell the future. The word had been in general use some time before Pliny (c. A.D. 5o) employed it first in an anatomical sense. It was applied to the bellies of pregnant swine, whose mammary glands were swollen with milk, and • which were eaten as delicacies. Cicero and the comedy writers transferred the word to the human body, using it in a slighting sense with reference to the paunch of the gourmand.",
+            origin: "Latin"
+        }, {
+            id: 3,
+            term: "Abducens",
+            derivation: "Ab (L) away from, and ducere (L) = to lead. Hence A bductor, a muscle bringing about move-ment away from the mid-line. The Abducent nerve derives its name from the fact that it supplies the lateral rectus muscle, which turns the eyeball outwards. The lateral rectus muscle at one time was known as the musculus amatorius because of its action in producing the sidelong glance of lovers.",
+            origin: "Latin"
+
+        }, {
+            id: 4,
+            term: "Abernethy, John.",
+            derivation: "I764-183T. English surgeon and anatomist. As a student at St. Bartholomew's Hospital he received lectures from Percival Pott and John Hunter. Later he became Professor of Surgery and Anatomy to the Royal College of Surgeons (1814) and Surgeon to St. Bartholomew's Hospital. He did no original work of great value, but was a brilliant lecturer. His fame and reputation arose more from his character and personality than from any researches in anatomy or surgery.",
+            type: "English surgeon and anatomist",
+            lifespan: "1764-1831"
+        }, {
+            id: 5,
+            term: "Abernethy's fascia",
+            derivation: "that part of the pelvic W-covering the external iliac vessels."
+        }, {
+            id: 6,
+            term: "Aberrant",
+            derivation: "Ab (L) = away from, and errare (L) = to stray. An aberrant type shows divergence from the normal. Vas aberrans = an anomalous blood veaseb applied especially to a branch of the brachial artery arising high up in the arm and usually supplanting the radial artery in a greater or lesser degree.",
+            origin: "Latin"
+        }, {
+            id: 7,
+            term: "Abortion",
+            derivation: "Aborior (L) = to set, e.g. setting of the sun. In late Latin—abortare—to abort.",
+            origin: "Latin"
+        }, {
+            id: 8,
+            term: "Absorption",
+            derivation: "A bsoi'beo (L) = I swallow up; devour. Hence the sucking up or taking in of materials.",
+            origin: "Latin"
+        }, {
+            id: 9,
+            term: "Acanthion",
+            derivation: "Akantha (Gk) = a thorn. A point at the base of the anterior nasal spine.",
+            origin: "Greek"
+        }, {
+            id: 10,
+            term: "Accessory",
+            derivation: "Accedere (L) = to be added to, from cede re (L) = to yield. Hence supplementary. Although the accessory nerve is structurally distinct from the vagus nerve, it is functionally accessory to it. Cf. Flexor accessorius.",
+            origin: "Latin"
+        }, {
+            id: 11,
+            term: "Acetabulum",
+            derivation: "Acetum (L) = vinegar, and -abulum dim. of -abrum (L) = a holder or receptacle. Any small spherical container, even a juggler's cup, was called acetabulum by the Romans, though it referred * in particular to a small vessel holding about two fluid ounces in which vinegar was placed at table. The word probably started as acceptabulum = a little receiver and became acetabulum by a translator’s mistake",
+            origin: "Latin"
+        }, {
+            id: 12,
+            term: "Achilles tendon",
+            derivation: ""
+        }
+    ]
+export default staticData
